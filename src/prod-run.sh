25ea8ba120d6f695a4d4b59bc0af56580158f5a4
epickiwi <me@epickiwi.fr>
2019-12-08 14:38:43 +0100

    Woopsi fix

--- src/prod-run.sh
+++ src/prod-run.sh
@@ -6,8 +6,8 @@ while ! pg_isready -U "$POSTGRES_USER" -h "$POSTGRES_HOST"; do
   sleep 1
 done
 
-IS_MIGRATING=true python manage.py migrate --noinput
+python manage.py migrate
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 gunicorn bdecesi.wsgi --bind=0.0.0.0 $@
