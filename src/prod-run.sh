f60fcc6846e23fb15332beb34b47927748378601
EpicKiwi <me@epickiwi.fr>
2020-03-02 19:27:49 +0100

    Ajout de logs de production

--- src/prod-run.sh
+++ src/prod-run.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 set -e
 
+echo "Webpack build"
 cd .. && npm run build
 
+echo "Collecting statics"
 cd src/ && python manage.py collectstatic --noinput
 
+echo "Checking Postgres"
 while ! pg_isready -U "$POSTGRES_USER" -h "$POSTGRES_HOST"; do
   echo "Waiting for postgres to be ready..."
   sleep 1
 done
 
+echo "Checking elasticsearch"
 while ! curl $ELASTIC_HOST >> /dev/null; do
   echo "Waiting for elasticsearch to be ready..."
   sleep 1
 done
 
+echo "Applying migrations"
 python manage.py migrate
+
+echo "Rebuilding elasticsearch indexes"
 python manage.py search_index --rebuild -f
 
+echo "Starting server"
 gunicorn bdecesi.wsgi --bind=0.0.0.0 $@
