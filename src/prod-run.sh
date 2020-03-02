3a45607064531b22a4178d3f5f8454c809d0a2c3
EpicKiwi <me@epickiwi.fr>
2020-03-02 21:45:59 +0100

    fix : Reactivation des systèmes de sécurité elasticsearch

--- src/prod-run.sh
+++ src/prod-run.sh
@@ -24,7 +24,6 @@ python manage.py migrate
 
 echo "Rebuilding elasticsearch indexes"
 python manage.py search_index --rebuild -f
-curl -XPUT -H "Content-Type: application/json" http://$ELASTIC_HOST/_all/_settings -d '{"index.blocks.read_only_allow_delete": false}'
 
 echo "Starting server"
 gunicorn bdecesi.wsgi --bind=0.0.0.0 $@
